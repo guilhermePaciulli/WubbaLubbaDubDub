1d512338011c6ba1e3ae4c1ce3e585654e172608
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2018-12-26 17:40:00 -0200

    characters dao tests

--- WubbaLubbaDubDub/Source/Services/Data Access Objects/CharactersDAO.swift
+++ WubbaLubbaDubDub/Source/Services/Data Access Objects/CharactersDAO.swift
@@ -10,12 +10,12 @@ import Foundation
 
 class CharactersDAO {
     
-    let shared = CharactersDAO()
+    static let shared = CharactersDAO()
     
     private init() {}
     
-    func fetch(atPage page: Int, completion: @escaping ResultCallback<GetCharacters.Response>) {
-        APIClient.shared.send(GetCharacters(atPage: page), completion: completion)
+    func fetch(withName name: String? = nil, status: Status? = nil, species: String? = nil, type: String? = nil, andGender gender: Gender? = nil, atPage page: Int = 1, completion: @escaping ResultCallback<GetCharacters.Response>) {
+        APIClient.shared.send(GetCharacters(withName: name, status: status, species: species, type: type, andGender: gender, atPage: page), completion: completion)
     }
     
     func fetch(byID characters: [Character], completion: @escaping ResultCallback<SelectCharacter.Response>) {
