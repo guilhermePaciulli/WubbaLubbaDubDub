52b205f5547d8556b5a02ef7997f76ed443978a9
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2019-01-04 11:17:42 -0200

    protocols implementation started

--- WubbaLubbaDubDub/Source/Modules/Characters List/CharactersListProtocols.swift
+++ WubbaLubbaDubDub/Source/Modules/Characters List/CharactersListProtocols.swift
@@ -10,24 +10,28 @@ import Foundation
 
 // MARK: - Router
 protocol CharactersListRouterProtocol: class {
-
+    func presentCharacter(character: Character)
 }
 
 // MARK: - Interactor
 protocol CharactersListInteractorInputProtocol {
-
+    func fetchCharacters()
 }
 
 // MARK: - Presenter
 protocol CharactersListPresenterInputProtocol: class {
-
+    func numberOfCharacters() -> Int
+    func character(at index: Int) -> Character
+    func didSelectCharacter(_ character: Character)
 }
 
 protocol CharactersListInteractorOutputProtocol: class {
-
+    func handleSuccessFetchingCharacters(with results: [Character])
+    func handleFailureFetchingCharacters(with error: String)
 }
 
 // MARK: - View
 protocol CharactersListPresenterOutputProtocol: class {
-
+    func didFetchCharacters()
+    func presentError(message: String)
 }
