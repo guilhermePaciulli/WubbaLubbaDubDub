c81924058f0da4034df0590ace5d49d563eba0dc
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2019-01-07 11:25:52 -0200

    more design details

--- WubbaLubbaDubDub/Source/Modules/Characters List/View/CharactersListView.swift
+++ WubbaLubbaDubDub/Source/Modules/Characters List/View/CharactersListView.swift
@@ -15,6 +15,7 @@ class CharactersListView: UIViewController, CharactersListPresenterOutputProtoco
     
     // MARK: - Outlets
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     // MARK: - Properties
     private var reuseIdentifier = "CharacterCell"
@@ -22,21 +23,30 @@ class CharactersListView: UIViewController, CharactersListPresenterOutputProtoco
     // MARK: - Override methods
 	override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.collectionView.delegate = self
         self.collectionView.prefetchDataSource = self
         self.collectionView.dataSource = self
         
         self.presenter.fetchFirstCharacters()
+        self.startLoadingEntireCollectionView()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.968627451, green: 0.9411764706, blue: 0.4784313725, alpha: 1)
+        self.navigationController?.navigationBar.tintColor = #colorLiteral(red: 0.968627451, green: 0.9411764706, blue: 0.4784313725, alpha: 1)
+        self.navigationController?.navigationBar.isTranslucent = false
     }
 
     // MARK: - CharactersListPresenterOutputProtocol
     func didFetchCharacters(atIndexes indexes: [IndexPath]?) {
         DispatchQueue.main.async {
             self.collectionView.reloadData()
+            if self.activityIndicator.isAnimating {
+                self.stopLoadingEntireCollectionView()
+            }
         }
     }
     
@@ -51,6 +61,15 @@ class CharactersListView: UIViewController, CharactersListPresenterOutputProtoco
     }
 
 	// MARK: - Private Methods
+    func startLoadingEntireCollectionView() {
+        self.activityIndicator.isHidden = false
+        self.activityIndicator.startAnimating()
+    }
+    
+    func stopLoadingEntireCollectionView() {
+        self.activityIndicator.isHidden = true
+        self.activityIndicator.stopAnimating()
+    }
 
 }
 
